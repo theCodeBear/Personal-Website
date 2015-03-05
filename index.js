41e06d83ca2229d49a7a4c8aa637930f5d89460a
Todd Kronenberg <toddkronenberg@gmail.com>
2015-03-05 01:49:21 -0800

    Got the main img display working

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 $(function() {
 
-  var titles = ['Full Stack Web Dev', 'Node.js', 'AngularJS', 'Hapi.js', 'Express.js', 'Bootstrap', 'MongoDB', 'HTML 5', 'CSS 3', 'jQuery'];
+  var titles = ['Full Stack Web Developer', 'Node.js', 'AngularJS', 'Hapi.js', 'Express.js', 'Bootstrap', 'MongoDB', 'HTML 5', 'CSS 3', 'jQuery'];
+  var logos = ['', 'img/node-logo-lg.png', 'img/angular-logo.png', 'img/hapi-logo.png', 'img/express-logo.png', 'img/bootstrap-logo.png', 'img/mongo-db-logo.png', 'img/html5-logo.png', 'img/css3-logo-1.png', 'img/jquery-logo.png'];
   var i=0;
   $('#title').text(titles[i]);
 
   $('#title').mouseenter(function() {
     if (i === titles.length-1) i=-1;
     $('#title').text(titles[++i]);
-    $('body').css('background', 'url(img/html5-logo.png) no-repeat center center fixed');
+    // $('#logos').attr('src', logos[i]);
+    $('#title').attr('display','none');
+    $('#mainDisplay').css('background', 'url(' + logos[i] + ') no-repeat center center fixed');
   });
 });
