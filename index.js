39740aafe8c4ad22f059d886f8134c0450408537
Todd Kronenberg <toddkronenberg@gmail.com>
2015-03-05 00:32:15 -0800

    Got some much stuff done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 $(function() {
 
-  $('#developerTitle').hover(function() {
-    $(this).css("color", "blue");
-    $('#title').text('Node.js');
-  }, function() {
-    $(this).css("color", "black");
-    $('#title').text('Full-Stack Web');
+  var titles = ['Full Stack Web Dev', 'Node.js', 'AngularJS', 'Hapi.js', 'Express.js', 'Bootstrap', 'MongoDB', 'HTML 5', 'CSS 3', 'jQuery'];
+  var i=0;
+  $('#title').text(titles[i]);
+
+  $('#title').mouseenter(function() {
+    if (i === titles.length-1) i=-1;
+    $('#title').text(titles[++i]);
+    $('body').css('background', 'url(img/html5-logo.png) no-repeat center center fixed');
   });
-
-
 });
