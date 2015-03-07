b2fc085a3c87bdcf5211c398f05711e5dbd013ef
Todd Kronenberg <toddkronenberg@gmail.com>
2015-03-07 01:16:58 -0800

    Sexy bars sticking to top of screen!

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ $(function() {
 
   $window.scroll(function() {
     $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
+    if ($window.scrollTop() > elTop) {
+      $('#replacementSpace').css('height', $('#stickyDiv').height());
+      $('.icons').css('backgroundColor', 'rgba(255,255,255,1)');
+    }
+    if ($window.scrollTop() < elTop) {
+      $('#replacementSpace').css('height', '0');
+      $('.icons').css('backgroundColor', 'rgba(255,255,255,0.3)');
+    }
   });
 
 });
