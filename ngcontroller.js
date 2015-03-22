b962f2022d547f418a22fc0b7724e1ccc2b0c1ec
Todd Kronenberg <toddkronenberg@gmail.com>
2015-03-22 13:17:12 -0700

    Switched project pages to modals, made hire form Got rid of the project pages for Sigma Shooter and the Hill Cipher and now the links just go to bootstrap modals that show the videos of those two projects and a link to their GitHub repos. I made the form for the Hire Me button and put in the code to send an email to me from the form but for some reason that ajax call fails right now. Made it so the form clears if the user hits the close button of the 'x' button to close the modal.

--- ngcontroller.js
+++ ngcontroller.js
@@ -1,6 +1,6 @@
 angular.module('krone', [])
 
-.controller('main', ['$scope', function($scope) {
+.controller('main', ['$scope', '$http', function($scope, $http) {
   $scope.projects = [
       {
       title: 'Eddie Exchange',
@@ -22,20 +22,79 @@ angular.module('krone', [])
       description: "Demo Drawing Project to learn how to use the HTML5 Canvas.",
       picture: "img/CanvasDraw_screenShot.png",
       techs: ["JavaScript", "jQuery", "HTML5 Canvas"]
-    },
-    {
-      title: "Sigma Shooter",
-      url: "projects/SigmaShooter.html",
-      description: "Console Shoot 'em Up game created as my final project in a college Assembly Language course. Written using the MASM Assembler in Visual Studio 2010.",
-      picture: "img/SigmaShooter_screenShot.png",
-      techs: ["Assembly Language"]
-    },
-    {
-      title: "Hill Cipher Encryption",
-      url: "projects/hillCipher.html",
-      description: "Written in C++ using the Linear Algebra-related cryptographic Hill Cipher. Plain text is converted into a matrix, then encrypted using a randomly generated 3x3 matrix. The decryption involves calculating the inverse of the encryption matrix and applying it to the cipher text.",
-      picture: "img/LinAlg_screenShot.png",
-      techs: ["C++"]
-    }
+    }//,
+    // {
+    //   title: "Sigma Shooter",
+    //   url: "projects/SigmaShooter.html",
+    //   description: "Console Shoot 'em Up game created as my final project in a college Assembly Language course. Written using the MASM Assembler in Visual Studio 2010.",
+    //   picture: "img/SigmaShooter_screenShot.png",
+    //   techs: ["Assembly Language"]
+    // },
+    // {
+    //   title: "Hill Cipher Encryption",
+    //   url: "projects/hillcipher.html",
+    //   description: "Written in C++ using the Linear Algebra-related cryptographic Hill Cipher. Plain text is converted into a matrix, then encrypted using a randomly generated 3x3 matrix. The decryption involves calculating the inverse of the encryption matrix and applying it to the cipher text.",
+    //   picture: "img/LinAlg_screenShot.png",
+    //   techs: ["C++"]
+    // }
   ];
+
+  $scope.clearForm = function() {
+    $scope.email = '';
+    $scope.name = '';
+    $scope.msgTitle = '';
+    $scope.msg = '';
+  }
+
+  $scope.hireMe = function() {
+    // var email = {
+    //   key: 'Ds4ZZAGuZEOplfx2G54uAg',
+    //   message: {
+    //     html: '<p> Example HTML content </p>',
+    //     text: $scope.msg,
+    //     subject: $scope.msgTitle,
+    //     from_email: $scope.email,
+    //     from_name: $scope.name,
+    //     to: [{
+    //       email: 'toddkronenberg@gmail.com',
+    //       name: 'Todd Kronenberg',
+    //       type: 'to'
+    //     }]
+    //   }
+    // };
+    console.log($scope.msg);
+    console.log($scope.msgTitle);
+    console.log($scope.email);
+    console.log($scope.name);
+    var email = {
+      key: 'Ds4ZZAGuZEOplfx2G54uAg',
+      message: {
+        from_email: '$scope.email',
+        to: [{
+          email: 'toddkronenberg@gmail.com',
+          type: 'to'
+        }],
+        autotext: 'true',
+        html: '<p> Example HTML content </p>'
+      }
+    }
+
+    // $.ajax({
+    //   type: "POST",
+    //   url: "https://mandrillapp.com/api/1.0/messages/send.json",
+    //   data: email,
+    //   success: function(msg){
+    //     alert('email sent', msg);
+    //   }, error: function(XMLHttpRequest, textStatus, errorThrown) {
+    //     alert('error sending email');
+    //   }
+    // });
+
+    $http.post('https://mandrillapp.com/api/1.0/messages/send.json', {data: email}).success(function(msg) {
+      alert('email sent', msg);
+    }).error(function(msg) {
+      alert('error sending email', msg);
+    });
+  };
+
 }]);
